9726b0cac89ba906912d91fb052fcb26dc59d358
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-10-08 09:39:19 -0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -3,10 +3,10 @@
 const { MongoClient } = require("mongodb");
 const { mongodb_url } = require("../config");
 
-var cachedData = new Map(); // mappings of tg <->whatsapp chatIDS
-var replyIDSTG = new Map(); // mapping to store tgMessageID -> whatsApp msgID
-var replyIDSWhatsAPP = new Map(); // mapping to store whatsApp msgID -> tgMessageID
-var pmEnabled = new Map(); // mapping for PM log status
+var cachedData = new Map(); // mappings of tg <-> whatsapp chatIDS // mapeamentos do TG <-> IDs de bate-papo do whatsapp
+var replyIDSTG = new Map(); // mapping to store tgMessageID -> whatsApp msgID // mapeamento para armazenar tgMessageID -> ID da mensagem do WhatsApp
+var replyIDSWhatsAPP = new Map(); // mapping to store whatsApp msgID -> tgMessageID // mapeamento para armazenar msgID do WhatsApp -> tgMessageID
+var pmEnabled = new Map(); // mapping for PM log status // mapeamento para status de log de PM ( privado do bot )
 
 const database = async (collection) => {
   var conn = await MongoClient.connect(mongodb_url, {

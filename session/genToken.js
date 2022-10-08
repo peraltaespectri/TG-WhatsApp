ada467fa192fa51d021cd8f0a75633a79e713a7a
peraltaespectri <113333418+peraltaespectri@users.noreply.github.com>
2022-10-08 09:41:01 -0300

    Update genToken.js

--- session/genToken.js
+++ session/genToken.js
@@ -18,27 +18,27 @@ const rl = readline.createInterface({
 });
 
 rl.question(
-  "Enter password to encrypt session (You need to put this in ENV): ",
+  "Digite a senha para criptografar a sessão (Você precisa colocar isso em ENV - SESSION_KEY): ",
   (answer) => {
     password = answer;
-    console.log("Password set to:", password);
-    console.log("Generating QR Code...");
+    console.log("Senha definida como:", password);
+    console.log("Gerando QR Code...");
     rl.close();
     client.initialize();
   }
 );
 
 client.on("qr", (qr) => {
-  console.log(`Scan this QR Code and copy the JSON\n`);
+  console.log(`Digitalize este QR Code e copie o JSON\n`);
   qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
   client.destroy();
-  console.log("Please wait...");
-  // wait because filesystem is busy
+  console.log("Por favor, Espere...");
+  // espere porque o sistema de arquivos está ocupado
   setTimeout(async () => {
-    console.log("Session has been created");
+    console.log("A Sessão foi criada com Sucesso");
     await write(password);
     process.exit();
   }, 3000);
